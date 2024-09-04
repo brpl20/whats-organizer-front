f133ee9c8c9fe08b87fc88ec9da9980a058eb4da
Bruno Pellizzetti <pelli.br@gmail.com>
2024-09-03 22:34:05 -0300

    fix bugs, audios, images, pending videos and pdfs - created error message - improved css etc etc tc

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,13 @@
 <script>
     //import WhatsOrganizer from '$lib/MainPage.svelte';
     //import ZipProcessor from '$lib/ZipProcessor.svelte';
-    //import Main from '$lib/Main.svelte'
+    import Main from '$lib/Main.svelte'
     import Pre from '$lib/Pre.svelte'
 </script>
   
 <Pre />
+
+
 <!-- <Pre /> -->
-<!-- <Main /> -->
+<Main />
 <!-- <WhatsOrganizer /> -->
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,27 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+  plugins: [sveltekit()],
+  optimizeDeps: {
+    include: ['jszip', 'mammoth']
+  },
+  build: {
+    commonjsOptions: {
+      include: [/node_modules/]
+    }
+  },
+  ssr: {
+    noExternal: ['pdfjs-dist']
+  }
 });
+
+// import { sveltekit } from '@sveltejs/kit/vite';
+// import { defineConfig } from 'vitest/config';
+
+// export default defineConfig({
+// 	plugins: [sveltekit()],
+// 	test: {
+// 		include: ['src/**/*.{test,spec}.{js,ts}']
+// 	}
+// });
